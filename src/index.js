5e770a1cf4972ffb22b383319212358794d78219
John Paul Machahuay <johnmachahuay@EJs-MacBook-Pro.local>
2017-01-20 12:17:58 -0500

    Added navBar component.

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+ } from 'react-native';
+
+ // Components
+ import NavBar from './components/navBar';
+
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{width: 100}}>Hello world</Text>
+        <NavBar
+          title="Inicio"
+          leftText = "1"
+          rightText = "2"
+          onLeftPress={ () => {
+
+          }}
+          containerStyle={{backgroundColor: '#303030'}}
+          colorText='#eee' />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center', // align horizontal
+    justifyContent: 'center', // align vertical
+  }
+})
